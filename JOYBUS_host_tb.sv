42da4832a8faa439acaf6fa2082c9dbde5bf3f0e
Julien de Castelnau <tripod320@gmail.com>
2021-12-18 20:41:37 -0600

    expanded testbench for host

--- JOYBUS_host_tb.sv
+++ JOYBUS_host_tb.sv
@@ -12,7 +12,7 @@ import tb_tasks::*;
     reg cntlr_test_write;
     reg JB_RX;
 
-task automatic send_dummy_resp(ref RX);
+task automatic send_rand_resp(ref RX, output [31:0] resp);
     begin
     // status byte
     bit0(RX);
@@ -23,53 +23,30 @@ task automatic send_dummy_resp(ref RX);
     bit1(RX);
     bit0(RX);
     bit1(RX);
-    // some made up data
-    bit0(RX);
-    bit1(RX);
-    bit1(RX);
-    bit1(RX);
-    bit0(RX);
-    bit0(RX);
-    bit1(RX);
-    bit1(RX);
-    // second byte
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    bit1(RX);
-    // third byte
-    bit0(RX);
-    bit0(RX);
-    bit1(RX);
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    bit0(RX);
-    // fourth byte
-    bit0(RX);
-    bit0(RX);
-    bit1(RX);
-    bit0(RX);
-    bit0(RX);
-    bit1(RX);
-    bit0(RX);
-    bit1(RX);
+    resp = $urandom();
+    $display("Sending %h", resp);
+    for (int i = 31; i >= 0; i--)
+        if (resp[i]) begin
+            bit1(RX);
+        end else begin
+            bit0(RX);
+        end
     bit_stop(RX);
     end
 endtask
 
     JOYBUS_host iDUT (.*);
 
+    integer cnt;
+    logic [31:0] resp;
+
     initial begin
         rst_n = 1;
         clk = 0;
         cntlr_test_write = 0;
         JB_RX = 0;
+        
+        cnt = 0;
 
         @(posedge clk);
         rst_n = 0;
@@ -79,13 +56,14 @@ endtask
 
         @(posedge clk);            
 
+        repeat (3) begin
         wait4sig(clk, iDUT.tx_done, 2000000);
 
         fork
             begin: send_data
                 $display("TRANSFERRING CONTROL.. time=%t", $time());
                 cntlr_test_write = 1;
-                send_dummy_resp(JB_RX);
+                send_rand_resp(JB_RX, resp);
                 cntlr_test_write = 0;
                 repeat(1000) @(posedge clk);
                 $display("Error timeout for waiting for rx_done");
@@ -100,6 +78,12 @@ endtask
         repeat (2) @(posedge clk);
         $display("Controller values received:");
         $display("A: %b B: %b Z: %b", btn_A, btn_B, btn_Z);
+        assert(btn_A == resp[31]);
+        assert(btn_B == resp[30]);
+        assert(btn_Z == resp[29]);
+        $display("Cycle: %d", cnt);
+        cnt = cnt + 1;
+        end 
 
         $display("YAHOO! Test passed..");
         $finish();
